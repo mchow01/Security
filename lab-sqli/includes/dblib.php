e9ac2b8d7112f5931ff1eededb2f70a837d1a4cc
Ming Chow <mchow@cs.tufts.edu>
2018-02-24 23:43:04 -0500

    Updated dblib.php to support PHP 7

--- lab-sqli/includes/dblib.php
+++ lab-sqli/includes/dblib.php
@@ -1,40 +1,39 @@
 <?php
+	// Database schema: CREATE TABLE users (id INT(9) NOT NULL AUTO_INCREMENT, login VARCHAR(60) NOT NULL, password TEXT NOT NULL, PRIMARY KEY (id));
+	// Be sure to add a record: INSERT INTO users (login, password) VALUES ('SOME USERNAME HERE','SOME PASSWORD HERE');
 	$myUserName = '';
 	$myPassword = '';
 	$myDBName = '';
 	$myHost = '';
-	$db = mysql_connect($myHost, $myUserName, $myPassword);
+	$db = mysqli_connect($myHost, $myUserName, $myPassword);
 	if (!$db) {
-		die('Cannot connect to the database: ' . mysql_error());
+		die('Cannot connect to the database: ' . mysqli_error());
 	}
 	else {
-		mysql_select_db($myDBName) or die("Unable to select database");
+		mysqli_select_db($db, $myDBName) or die("Unable to select database");
 	}
 
-	function getDB()
-	{
+	function getDB() {
 		global $db;
 		return $db;
 	}
 
-	function getLogin ($login, $password)
-	{
+	function getLogin ($login, $password) {
 		global $db;
-		$result = mysql_query("SELECT id FROM users WHERE login = '" . mysql_real_escape_string($login) . "' and password = '" . $password . "'");
-		#$result = mysql_query("SELECT id FROM users WHERE password = SHA1('" . $password . "') and login = '" . $login . "'");
+		$result = mysqli_query($db, "SELECT id FROM users WHERE login = '" . mysqli_real_escape_string($db, $login) . "' and password = '" . $password . "'");
 		if (!result) {
-			$message  = 'Invalid query: ' . mysql_error() . "\n";
+			$message  = 'Invalid query: ' . mysqli_error($db) . "\n";
 			$message .= 'Whole query: ' . $query;
 			die($message);
 		}
 		else {
 			$row = array();
-			$row = mysql_fetch_array($result);
+			$row = mysqli_fetch_array($result);
 			if (!empty($row)) {
 				return $row;
 			}
 		}
-		mysql_free_result($result);
+		mysqli_free_result($result);
 		return false;
 	}
 ?>
